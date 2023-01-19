1a5b6bfe4a950c6b6b809a1cf5bbec586fd641d9
Aditya Nagori <aditya.mits13@gmail.com>
2023-01-19 13:39:51 -0600

    cvscore api
    api functionality added for cv score calculation.

---
+++ cvscore_api.py
@@ -0,0 +1,46 @@
+import sklearn
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import figure
+import numpy as np
+#%matplotlib inline
+
+from sklearn import set_config
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
+
+from sksurv.datasets import load_gbsg2
+from sksurv.preprocessing import OneHotEncoder
+from sksurv.ensemble import RandomSurvivalForest
+from flask import Flask, request, url_for, redirect, render_template, jsonify
+import joblib
+import json
+
+
+
+app = Flask(__name__)
+
+rsf = joblib.load("RF_compressed.joblib")
+
+
+#cols = ['Age', 'SBP', 'DBP', 'BMI',
+#       'CurrentSmoking', 'HTN', 'Diabetes', 'HTNmeds', 'SCreat', 'HDL', 'LDL',
+#       'TotChol', 'Trigs', 'FPG', 'NTproBNP', 'Troponin', 'CRP', 'QRS', 'QTc',
+#       'LVHcv']
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    jdata = request.json
+    jdata = json.dumps(jdata)
+    data_unseen = pd.read_json(jdata, orient = 'records' )
+    prediction = rsf.predict(data_unseen)
+    output = prediction.round(decimals= 2)
+    return jsonify({"prediction":list(output)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+    
+    
